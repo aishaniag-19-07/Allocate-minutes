2179aa919d28a88079bf8a01c451bae749b81fa8
aishaniag-19-07 <aishaniag2410@gmail.com>
2026-09-18 15:27:33 +0530

    add chge

--- frontend/auth.js
+++ frontend/auth.js
@@ -24,7 +24,7 @@
 // CONFIGURATION
 // ============================================================
 
-// const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://127.0.0.1:8000";
 const SESSION_KEY = "am_current_user";
 
 
